0cb067af63eaf6753e1e6ca2e7dec672b749876e
Carmine Paolino <carmine@paolino.me>
2011-06-26 10:10:55 +0200

    Back to an emacsy emacs

--- carmine.el
+++ carmine.el
@@ -13,13 +13,6 @@
   (set-frame-size (car (frame-list)) 142 40) ;; characters wide, lines tall
   (set-frame-position (car (frame-list)) 17 33)) ;; x y
 
-;; Disable fringes
-(fringe-mode 0)
-
-;; Enable line numbers
-(global-linum-mode 1)
-(setq linum-format " %d ")
-
 ;; TextMate-like keybindings
 (textmate-mode)
 
